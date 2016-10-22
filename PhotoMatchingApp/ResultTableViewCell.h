0a552ab89e221f807c640014f344a245290659bb
takumi314 <isospin_symmetry@me.com>
2016-10-22 14:52:58 +0900

    Set Outlet titleLabel, photoImageView

--- PhotoMatchingApp/ResultTableViewCell.h
+++ PhotoMatchingApp/ResultTableViewCell.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ResultTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *resultPhotoImageView;
 
 @end
