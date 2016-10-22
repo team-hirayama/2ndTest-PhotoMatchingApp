6ca3efd556771a6c414a379b7ddff10e880279c8
takumi314 <isospin_symmetry@me.com>
2016-10-22 14:49:18 +0900

    Add ResultTableViewCell class

---
+++ PhotoMatchingApp/ResultTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ResultTableViewCell.h
+//  PhotoMatchingApp
+//
+//  Created by NishiokaKohei on 2016/10/22.
+//  Copyright © 2016年 Kohey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultTableViewCell : UITableViewCell
+
+@end
---
+++ PhotoMatchingApp/ResultTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ResultTableViewCell.m
+//  PhotoMatchingApp
+//
+//  Created by NishiokaKohei on 2016/10/22.
+//  Copyright © 2016年 Kohey. All rights reserved.
+//
+
+#import "ResultTableViewCell.h"
+
+@implementation ResultTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
